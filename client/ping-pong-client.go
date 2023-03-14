d71856e022580ce86867d3ebcb0e453117238ecf
Pavel Shakhlovich <pavel_shakhlovich@epam.com>
2023-03-14 22:36:22 +0400

    generate stubs

--- client/ping-pong-client.go
+++ client/ping-pong-client.go
@@ -1,36 +1,35 @@
-package client
+package main
 
 import (
-    "context"
-    "log"
-    "time"
+	"context"
+	"log"
+	"time"
 
-    "google.golang.org/grpc"
-
-    pb "github.com/Altmerian/go-gRPC-client/proto"
+	pb "github.com/Altmerian/go-gRPC-client/proto"
+	"google.golang.org/grpc"
 )
 
 func main() {
-    // Set up a connection to the server.
-    conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
 
-    // Create a gRPC client instance.
-    client := pb.NewPingPongServiceClient(conn)
+	// Create a gRPC client instance.
+	client := pb.NewPingPongServiceClient(conn)
 
-    // Call the ping-pong method.
-    req := &pb.PingRequest{
-        Message:   "hello",
-        Timestamp: time.Now().UnixNano(),
-    }
-    resp, err := client.PingPong(context.Background(), req)
-    if err != nil {
-        log.Fatalf("could not ping-pong: %v", err)
-    }
+	// Call the ping-pong method.
+	req := &pb.PingRequest{
+		Message:   "Ping",
+		Timestamp: time.Now().UnixMilli(),
+	}
+	resp, err := client.PingPong(context.Background(), req)
+	if err != nil {
+		log.Fatalf("could not ping-pong: %v", err)
+	}
 
-    // Print the response.
-    log.Printf("ping-pong response: %v", resp)
+	// Print the response.
+	log.Printf("ping-pong response: %v", resp)
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,11 @@
-module example.com/go-gRPC-client
+module github.com/Altmerian/go-gRPC-client
 
 go 1.20
+
+require (
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.29.1
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -8,6 +13,4 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
-	google.golang.org/grpc v1.53.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 )
